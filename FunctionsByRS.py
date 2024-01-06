74edb3b9217e32cccb938bef2fa9fa16eda6280d
CyberBro12 <94747680+CyberBro12@users.noreply.github.com>
2024-01-06 20:51:03 +0530

    FunctionsByRS.py
    Version 1

--- FunctionsByRS.py
+++ FunctionsByRS.py
@@ -1,26 +1,28 @@
-
-class UseFul:
+class Func:
 
     @staticmethod
-    def animationtype(x):
+    def animationtype(x, z):
         import time as t
         y = list(x)
         for char in y:
-            t.sleep(0.25)
+            t.sleep(z)
             print(char, end="")
+
     @staticmethod
     def create(y):
         with open(y, "a") as f:
             pass
+
     @staticmethod
     def clearcmd():
         import os
         os.system('cls')
+
     @staticmethod
     def getip():
         import socket
         hostname = socket.gethostname()
         ip_address = socket.gethostbyname(hostname)
-        
+
         print("Hostname: ", hostname)
         print("IPv4 Address: ", ip_address)
